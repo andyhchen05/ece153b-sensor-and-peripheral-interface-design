ab4c6e9987f554d516da7e84aa8c0d5972e9d61f
Andy Chen <andyhchen05@gmail.com>
2026-06-04 12:28:16 -0700

    Add files via upload

---
+++ L4/L4C/src/LED.c
@@ -0,0 +1,24 @@
+/*
+ * ECE 153B - Winter 2021
+ *
+ * Name(s): Andy Chen, Karl Tizon
+ * Section: Monday 6PM
+ * Lab: 4C
+ */
+
+#include "LED.h"
+
+void LED_Init(void) {
+	// Enable GPIO Clocks
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+	
+	// Initialize Green LED
+	GPIOA->MODER &= ~GPIO_MODER_MODE5;
+	GPIOA->MODER |= GPIO_MODER_MODE5_0;
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT5;
+	GPIOA->OSPEEDR &= ~GPIO_OSPEEDR_OSPEED5;
+	GPIOA->OSPEEDR |= GPIO_OSPEEDR_OSPEED5_1;
+	GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD5;
+	GPIOA->ODR &= ~GPIO_ODR_OD5;
+}
+
